08055443aa09672ee8139d7289041aa859937a56
leothehero <leothehero@users.noreply.github.com>
2021-04-06 15:10:13 +0200

    Update Card.lua

--- Card.lua
+++ Card.lua
@@ -1,3 +1,5 @@
 local component = require("component")
 local writer = component.os_cardwriter
 local magReader = component.proxy("fc91d79f-5720-430c-a0d5-72b094262b99")
+local shell = require("shell")
+local args, opts = shell.parse(...)
